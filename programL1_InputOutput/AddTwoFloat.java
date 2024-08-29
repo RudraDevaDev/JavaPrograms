bad6a3118c753d67d448404b2a1a2d8df8bef631
RudraDevaDev <rudra2002g@gmail.com>
2024-08-30 00:15:12 +0530

    P1L1C1

---
+++ programL1_InputOutput/AddTwoFloat.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AddTwoFloat{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float var1,var2;
+        var1 = sc.nextInt();
+        var2 = sc.nextInt();
+        System.out.printf("%.3f",var1+var2)g;
+    }
+}
