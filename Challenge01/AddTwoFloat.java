677df44b063e8d6e8db4a8fd2d5e76a86702581c
RudraDevaDev <rudra2002g@gmail.com>
2024-08-30 00:41:37 +0530

    Final

--- Challenge01/AddTwoFloat.java
+++ Challenge01/AddTwoFloat.java
@@ -3,7 +3,9 @@ public class AddTwoFloat{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         float var1,var2;
+        System.out.println("Enter variable 1 value:");
         var1 = sc.nextInt();
+        System.out.println("Enter variable 2 value:");
         var2 = sc.nextInt();
         sc.close();
         float total = var1+var2;
